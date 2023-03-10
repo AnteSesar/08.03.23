37c6c4347b0b63917b6bab18ec5cb95de407222e
Ante <ante.sesar11@gmail.com>
2023-03-10 19:23:03 +0100

    end

--- 21 - OOP JavaScript - 1.dio/Pocetno/scripts.js
+++ 21 - OOP JavaScript - 1.dio/Pocetno/scripts.js
@@ -10,3 +10,35 @@ const quotes = [
 	'See? Zoomer.', 
 	'Bitchin.'
 ];
+
+// for (i=0; i <= quotes.length; i++){
+// 	console.log(quotes[i]);
+// }
+
+//1
+var proto1 = Object.getPrototypeOf(character);
+var proto2 = Object.getPrototypeOf(proto1);
+
+
+// console.log(proto1);
+// console.log(proto2);
+
+//2
+//var rndCnt = parseInt(Math.random()*10) % 4;
+//var rndCnt = Math.floor(Math.random()*quotes.length);
+// var rndCnt = parseInt(Math.random()*quotes.length);
+// console.log(rndCnt);
+// var rndVal = quotes[rndCnt];
+// console.log(rndVal);
+
+//3
+character.prototype = function(){
+	let rndCnt = parseInt(Math.random()*quotes.length);
+	//console.log(rndCnt);
+	let rndVal = quotes[rndCnt];
+	//console.log(rndVal);
+	return rndVal;
+}
+
+console.log(character.prototype());
+//console.log(proto1);
